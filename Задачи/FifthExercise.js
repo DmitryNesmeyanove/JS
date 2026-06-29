52b28a34daa7460a3e4b44131df9e8e2272191de
DmitryNesmeyanove <dmitry.nesmeyanov1@gmail.com>
2026-06-29 17:25:14 +0300

    Refactor: Изменен вывод с учетом интерполяции

--- Задачи/FifthExercise.js
+++ Задачи/FifthExercise.js
@@ -2,5 +2,5 @@ const name = 'Игорь';
 const food = ['бургеры', 'сок', 'чипсы', 'пиццу', 'пельмени'];
 
 for(let items of food){
-    console.log(name + " любит " + items);
+    console.log(`${name} любит ${items}`);
 }
